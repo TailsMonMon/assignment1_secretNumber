c188655fb02b50af27814cebd221b4dc714ad838
Monica Björk <m.bjork95@gmail.com>
2018-04-12 09:29:27 +0200

    Add project files.

---
+++ assignment1_secretNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1_secretNumber {
+    class Program {
+        static void Main(string[] args) {
+
+            Random randomerare = new Random();
+            int slump_tal = randomerare.Next(1,101);
+            Console.Write(slump_tal);
+
+            Console.ReadLine();
+        }
+    }
+}
