b0439594a02f5d69f625a6a5dcdf57d1895db72c
Monica Björk <m.bjork95@gmail.com>
2018-04-12 19:22:07 +0200

    Some finishing touches.

--- assignment1_secretNumber/Program.cs
+++ assignment1_secretNumber/Program.cs
@@ -8,35 +8,43 @@ namespace assignment1_secretNumber {
     class Program {
         static void Main(string[] args) {
 
+            //      DEFINITIONS AND MAKING OF RANDOMERARE
             Random randomerare = new Random();
             int slumpTal = randomerare.Next(1,101);
             //Console.WriteLine("Det slumpade talet är: {0}", slumpTal); // controll of the random number.
+            int gissning = 0;   // Defines the variable before the do-while-loop.
             int antalGissningar = 0;    // Defines and starts with 0 attemps.
 
+            //      INTRO
+            Console.WriteLine("=== Gissa nummeret ===");
             Console.Write("Gissa ett nummer mellan 1 och 100: ");
-            int gissning = Convert.ToInt32(Console.ReadLine());
-
+            
             do {
-                if(gissning > slumpTal+15 && gissning <= 100) {     // bigger than slumpTal+15 AND not bigger than 100.
-                    Console.Write("Gissa ett mindre tal: ");
+                //      INPUT
+                try {
                     gissning = Convert.ToInt32(Console.ReadLine());
                 }
-                else if(gissning > slumpTal && gissning <= slumpTal+15) {     // bigger than slumpTal AND smaller than slumpTal+15
+                catch(FormatException) {
+                    Console.WriteLine("Ojdå! Detdär är ingen siffra...");
+                }
+
+                //      OUTPUT
+                if(gissning > slumpTal+15 && gissning <= 100) {     // bigger than slumpTal+15 AND not bigger than 100.
+                    Console.Write("Gissa ett mindre tal: ");
+                }
+                else if(gissning > slumpTal && gissning <= slumpTal+15) {     // bigger than slumpTal AND smaller than slumpTal+15.
                     Console.Write("Nu är du nära, gissa ett mindre tal: ");
-                    gissning = Convert.ToInt32(Console.ReadLine());
                 }
                 else if(gissning < slumpTal-15 && gissning >= 1) {  // Smaller than slumpTal-15 AND not smaller than 1.
                     Console.Write("Gissa ett större tal: ");
-                    gissning = Convert.ToInt32(Console.ReadLine());
                 }
-                else if(gissning < slumpTal && gissning >= slumpTal-15) {  // Smaller than slumpTal AND bigger than slumpTal-15
+                else if(gissning < slumpTal && gissning >= slumpTal-15) {  // Smaller than slumpTal AND bigger than slumpTal-15.
                     Console.Write("Nu är du nära, gissa ett större tal: ");
-                    gissning = Convert.ToInt32(Console.ReadLine());
                 }
-                else {
+                else if(gissning != slumpTal) {     // Smaller than 0 OR bigger than 100. Does not need to be written since it's last in the else-if.
                     Console.Write("Gissa ett nummer mellan 1 och 100: ");   // Smaller than 0 OR bigger than 100.
-                    gissning = Convert.ToInt32(Console.ReadLine());
                 }
+                //      ENDING
                 antalGissningar++;  // Counts the number of attempts.
             } while(gissning != slumpTal);
             Console.WriteLine("Grattis! Du gissade rätt efter {0} försök!", antalGissningar);
