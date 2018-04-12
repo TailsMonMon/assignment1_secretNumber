8bd975a279021c9c19bb96021bce5b7b79f1a149
Monica Björk <m.bjork95@gmail.com>
2018-04-12 10:49:22 +0200

    Step two, I've made it possible to guess numbers and also get a nice reply.

--- assignment1_secretNumber/Program.cs
+++ assignment1_secretNumber/Program.cs
@@ -9,9 +9,27 @@ namespace assignment1_secretNumber {
         static void Main(string[] args) {
 
             Random randomerare = new Random();
-            int slump_tal = randomerare.Next(1,101);
-            Console.Write("Det slumpade nummeret är: {0}", slump_tal);
+            int slumpTal = randomerare.Next(1,101);
+            //Console.WriteLine("Det slumpade nummeret är: {0}", slumpTal); // controll of the random number
 
+            Console.Write("Gissa ett nummer mellan 1 och 100: ");
+            int gissning = Convert.ToInt32(Console.ReadLine());
+
+            do {
+                if(gissning > slumpTal && gissning < 101) {
+                    Console.Write("Gissa ett mindre tal: ");
+                    gissning = Convert.ToInt32(Console.ReadLine());
+                }
+                else if(gissning < slumpTal && gissning > 0) {
+                    Console.Write("Gissa ett större tal: ");
+                    gissning = Convert.ToInt32(Console.ReadLine());
+                }
+                else {
+                    Console.Write("Gissa ett nummer mellan 1 och 100: ");
+                    gissning = Convert.ToInt32(Console.ReadLine());
+                }
+            } while(gissning != slumpTal);
+            Console.WriteLine("Grattis! Du gissade rätt!");
             Console.ReadLine();
         }
     }
